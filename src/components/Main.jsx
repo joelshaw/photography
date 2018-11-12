cf4cceead72edfe794e65b219b361a8658aa34db
joelshaw <jshaw@rvadesignsolutions.com>
2018-11-11 23:03:59 -0500

    Created section on the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,9 +6,12 @@ export default class Main extends React.Component {
 
         return (
             <div id="wrapper">
-                <section id="hero__image">
-                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque dolor nisi reprehenderit illum laboriosam vitae deserunt eius suscipit quisquam consectetur sunt officia minus, dicta tempore, placeat praesentium labore et maiores!</p>
-
+                <section id="hero__section">
+                    <div id="hero__text">
+                        <h2>It's about telling the story.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque dolor nisi reprehenderit illum laboriosam vitae deserunt eius suscipit quisquam consectetur sunt officia minus, dicta tempore, placeat praesentium labore et maiores!</p>
+                    </div>
+                    <div id="hero__image"></div>
                 </section>
             </div>
         );
